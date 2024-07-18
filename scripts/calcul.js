9e1694e7ec66be979e9024789227b9ef9d3634c4
CFournier59 <clementfournier.c@gmail.com>
2024-07-18 21:08:43 +0200

    still have to configure poppup score

--- scripts/calcul.js
+++ scripts/calcul.js
@@ -1,6 +1,5 @@
 
 function lancerCalcul(startBtn,){
- 
     // transformation du bouton start en zone de saisie
     let divBlue = document.getElementById("divBlue")
     divBlue.innerHTML = `<p id= "i2">Utilise ton clavier pour taper la réponse et valide avec la touche "Entrée"</p><input maxlength = "2" type = "text" id="answer" name = "answer" class="a grey">`
@@ -10,7 +9,6 @@ function lancerCalcul(startBtn,){
     let score = 0
     // cacher les consignes
     for(let i = 1 ; i < 4 ; i++){
-   
         let hideText = document.querySelector(`#i${i}`);  
         hideText.classList.add("hide");
     }
@@ -20,72 +18,26 @@ function lancerCalcul(startBtn,){
     question.id = "question"
     question.innerHTML = `<div id = "1" class = "number">0</div><div id = "symbol" class = "number">X</div><div id = "2" class = "number">0</div>`
     main.prepend(question)
-    // génération et affichage du calcul à résoudre
-    let oneDiv = document.getElementById("1")
-    let one = randomNumber()
-    oneDiv.innerText = `${one}`
-    let twoDiv = document.getElementById("2")
-    let two = randomNumber()
-    twoDiv.innerText = `${two}`
-    let symbol = document.getElementById("symbol")
-    melangeur(modificateur)
-    symbol.innerText = modificateur[0]
-    symbolColor(symbol)
-    //ajout de l'event pour entrer les réponses
+    //génère le premier calcul
+    let goodAnswer = generateMath()
+    // mise en place de l'évenement "entrée"
     answer.addEventListener('keypress', function (e) {
-    
-        if (e.key === 'Enter') { 
-        
-            // résolution du calcul
-            let goodAnswer = null
-            switch (modificateur[0]){
-
-                case " + ":
-
-                    goodAnswer = one + two 
-                    console.log (goodAnswer)
-                    break
-
-                case " - ":
-
-                    goodAnswer = one - two
-                    console.log(one.innerText - two.innerText)
-                    break
-
-                case " X ":
-
-                    goodAnswer = one * two
-                    console.log(one.innerText * two.innerText)
-                    
-            }
-
+        if (e.key === 'Enter') {
             // comparaison des réponses
             if( Number(answer.value) === goodAnswer) {
-
                 score ++
                 checkScore(score)
-                one = randomNumber()
-                oneDiv.innerText = `${one}`
-                two = randomNumber()
-                twoDiv.innerText = `${two}`
-                melangeur(modificateur)
-                symbol.innerText = modificateur[0]
-                symbolColor(symbol)
-                console.log("ok")
-                 
+                goodAnswer = generateMath()              
             }
-
-            else{
-
-              
-
-            }
-            
+            else{             
+            }           
             answer.value = ""
             retournerMessageScore(score)
         }
-
     })
+    //mise en place du chrono
+    compteARebours(answer)
+
     
 
 
--- scripts/functions.js
+++ scripts/functions.js
@@ -2,215 +2,158 @@
 
 // la fonction qui indique le score 
 function retournerMessageScore(score){
-
     let divScore = document.querySelector(".score")
     divScore.innerText = `${score}`  
 }
-
 // la fonction qui vérifie le score et le personnalise
 function checkScore(score){
-
     let scorePicture = document.querySelector("#scoreCard img")
     let scoreDescription = document.querySelector("#description p")
     switch (score) {
-
-        case 8 : 
-        
+        case 8 :        
             scorePicture.srcset =  "images/calcul_2.png"
             scorePicture.src = scorePicture.srcset
             scoreDescription.innerText = "C'est pas pire, mais tu peux faire mieux."
             break
-
-        case 16 : 
-        
+        case 16 :         
             scorePicture.srcset =  "images/calcul_3.png"
             scorePicture.src = scorePicture.srcset
             scoreDescription.innerText = "Ça commence à cogiter là-dedans!"
             break
-
-        case 20 : 
-        
+        case 20 :         
             scorePicture.srcset =  "images/calcul_4.png"
             scorePicture.src = scorePicture.srcset
             scoreDescription.innerText = "Les neuronnes sont en feu!!"
             break
-
-        case 22 : 
-        
+        case 22 :         
             scorePicture.srcset =  "images/calcul_5.png"
             scorePicture.src = scorePicture.srcset
             scoreDescription.innerText = "T'es une p*tain de légende!!!"
             break
-
-        case 29 : 
-        
+        case 29 :        
             scorePicture.srcset =  "images/calcul_6.png"
             scorePicture.src = scorePicture.srcset
-            scoreDescription.innerText = "01001110 01 11101000110 11100000010100 10"
-              
+            scoreDescription.innerText = "01001110 01 11101000110 11100000010100 10"             
     }
-
 }
-
 // fonction qui mélange les mots ou les chiffres
 function melangeur(modificateur){
-
     for (let index = modificateur.length - 1; index > 0; index--) {
-
         const j = Math.floor(Math.random() * (index + 1));
         [modificateur[index], modificateur[j]] = [modificateur[j], modificateur[index]];
     }
-
     modificateur.slice(0, 100); 
     return modificateur
-
 }
-
-// La fonction qui colore le symbol
-
+// La fonction qui colore le symbole
 function symbolColor(symbol){
-
     switch(symbol.innerText){
-
         case "+":
-
-            symbol.classList.add("red")
+            symbol.className = "number red"
             break
-
         case "-":
-
-            symbol.classList.add("blue")
-            break
-
+            symbol.className = "number blue"
+            break   
         case "X":
-
-            symbol.classList.add("green")
-
-    }
-    
+            symbol.className = "number green"
+    }   
 }
 // Les fonctions qui signalent au besoin si la réponse est fausse
-
 function borderFail(){
-
     let zoneProposition = document.querySelector(".zoneProposition")
-    if(zoneProposition.className === "zoneProposition")
+    if(zoneProposition.className === "zoneProposition"){
         zoneProposition.classList.toggle("fail")
-
+    }
 }
-
 function borderSuccess(){
-
     let zoneProposition = document.querySelector(".zoneProposition")
     if(zoneProposition.className !== "zoneProposition")
         zoneProposition.classList.toggle("fail")
-
 }
-
+//la fonction qui génère le calcul à résoudre est sort la valeur réponse
+function generateMath(){
+    let oneDiv = document.getElementById("1")
+    let one = randomNumber()
+    oneDiv.innerText = `${one}`
+    let twoDiv = document.getElementById("2")
+    let two = randomNumber()
+    twoDiv.innerText = `${two}`
+    let symbol = document.getElementById("symbol")
+    melangeur(modificateur)
+    symbol.innerText = modificateur[0]
+    symbolColor(symbol)
+    let goodAnswer = null
+    switch (modificateur[0]){
+        case " + ":
+            return one + two 
+            break
+        case " - ":
+            return one - two
+            break
+        case " X ":
+            return one * two                   
+    }
+}
 // La fonction Email
-
 function afficherEmail(nom, email, score) {
-
     let mailto = `mailto:${email}?subject=Partage du score Azertype&body=Salut, je suis ${nom} et je viens de réaliser le score de ${score} sur le site d'Azertype !`
     location.href = mailto
-
 }
-
 // fonction qui vérifie le champ : "nom" du popup
-
 function validerNom(nom){
-
         let regexnom = new RegExp(".{2,}")
         if(!regexnom.test(nom)){
-
             throw new Error(`votre nom doit comporter au moins 2 caratères`)
-
-        }
-    
+        }   
 }
-
 // fonction qui vérifie le champ : "email" du  popup
-
 function validerEmail(email){
-
     let regexemail = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\\.[a-z0-9._-]+")
     if(!regexemail.test(email)){
-
         throw new Error(`le format de votre adresse mail n'est pas correct`)
-
-    }
-    
+    }    
 }
-
 // fonction qui affiche l'erreur
-
 function afficherErreur(message){
-
     let spanErreur = document.getElementById("erreurMessage")
     if(!spanErreur){
-
         let popup = document.querySelector(".popup")
         spanErreur = document.createElement("span")
         spanErreur.id = "erreurMessage"
-        popup.append(spanErreur)
-    
-    }
-   
+        popup.append(spanErreur)    
+    }  
     spanErreur.innerText = message
-
 }
-
 // fonction qui gère le formulaire du popup
-
 function gererFormulaire(score){
-
     try{
-
         let nom = document.getElementById("nom").value
         validerNom(nom)
         let email = document.getElementById("email").value
         validerEmail(email)
         afficherErreur("")
         afficherEmail(nom, email, score)  
-
     }
-
     catch(erreur){
-
         afficherErreur(erreur.message)
-
-    }
-   
+    }   
 }
-
 // fonction qui gère le compte à rebours
-
-function compteARebours(inputEcriture){
-
-    const main = document.querySelector("main")
-    let countDown = document.createElement("h1")
+function compteARebours(answer){
+    const time = document.querySelector(".time")
     let sec = 30
-    main.prepend(countDown)
-    let timer = setInterval(function(){
-            
-        countDown.innerHTML= sec
+    let timer = setInterval(function(){           
+        time.innerHTML= sec + "s"
         sec--
         //fin du compte à rebours
         if (sec < 0) {
-
             clearInterval(timer)
-            inputEcriture.disabled = true
+            answer.disabled = true
             afficherPopup()
-
-            }
-
-        }, 1000)
-
+        }
+    }, 1000)
 }
-
 // fonction qui sort un nombre entre 1 et 9 au hasard
-
 function randomNumber() {
     return Math.floor(Math.random() * 9);
-
 }
